d655dd0d8b5f74fa9a0c4306cf8385728050e6ef
Ty Busby <tyler.busby@averydennison.com>
2024-10-31 03:27:17 -0400

    Initial.

---
+++ postrise/src/main/java/org/adonix/postrise/App.java
@@ -0,0 +1,13 @@
+package org.adonix.postrise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
